c2436a7baae1274adf39f8f09d9a7b7aa6296ad8
adithgit <5285@mbcpeermade.com>
2022-07-11 16:11:45 +0530

    Finished

--- script.js
+++ script.js
@@ -2,6 +2,15 @@ const colorClasses = ['color1', 'color2', 'color3', 'color4', 'color2', 'color4'
 const tagNames = ['Trending', 'Sports', 'Fashion', 'Finance', 'Trade']
 document.addEventListener("DOMContentLoaded", main);
 
+function openMenu(){
+    document.getElementById("menu").classList.add('menu-open');
+    document.getElementById("menu").style.display = 'flex';
+
+}
+
+function closeMenu(){
+    document.getElementById("menu").classList.remove('menu-open');
+}
 function tagHelper(){
     if (window.innerWidth < 700) {
         const tags = document.querySelectorAll('.tag');
